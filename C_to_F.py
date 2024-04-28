2ddb4c62fc304dc3cfb4709110382e23d2f90c20
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-04-28 18:12:45 +0700

    Delete C_to_F.py

--- C_to_F.py
+++
@@ -1,6 +0,0 @@
-def convert(grad):
-    res = (grad * 1.8) + 32
-    return res
-cls = float(input('Введите значение температуры в градусах Цельсия: '))
-fhr = convert(cls)
-print(cls, 'градусов Цельсия эквивалентно', fhr.__round__(2), 'градусам Фаренгейта')
